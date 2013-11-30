501bc0013bd5f65456d3024268c33c03a764f474
Benoit Dumas <bntdumas@gmail.com>
2013-11-30 11:18:17 +0100

    Connect the admin widget to the databse admin class.
    Allow creation of the database, structure and static data from the SQL
    files

--- newSoftware/homeAdminTool/adminWindow.cpp
+++ newSoftware/homeAdminTool/adminWindow.cpp
@@ -1,11 +1,23 @@
 #include "adminWindow.h"
 #include "ui_adminWindow.h"
 
+#include "databaseAdmin.h"
+
+#include <QDateTime>
+#include <QThread>
+
 adminWindow::adminWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::adminWindow)
 {
     ui->setupUi(this);
+
+    // Register types
+    qRegisterMetaType<adminToolItem::messageType>("adminToolItem::messageType");
+
+    m_databaseAdmin = new databaseAdmin();
+    connect(m_databaseAdmin, SIGNAL(message(QString, adminToolItem::messageType)), this, SLOT(displayLogMessage(QString, adminToolItem::messageType)));
+    m_databaseAdmin->connectToDB();
 }
 
 adminWindow::~adminWindow()
@@ -13,17 +25,61 @@ adminWindow::~adminWindow()
     delete ui;
 }
 
-void adminWindow::on_actionDeleteTables_triggered()
+void adminWindow::displayLogMessage(const QString &text, const adminToolItem::messageType type)
 {
-    
+    switch (type) {
+    case adminToolItem::Info:
+        ui->txtLog->setTextColor(Qt::darkGray);
+        break;
+
+    case adminToolItem::Success:
+        ui->txtLog->setTextColor(Qt::darkGreen);
+        break;
+
+    case adminToolItem::Warning:
+        ui->txtLog->setTextColor(Qt::darkYellow);
+        break;
+
+    case adminToolItem::DatabaseError:
+        ui->txtLog->setTextColor(Qt::darkMagenta);
+        break;
+
+    case adminToolItem::SoftwareError:
+        ui->txtLog->setTextColor(Qt::darkRed);
+        break;
+    }
+    ui->txtLog->append(QStringLiteral("%1: %2").arg(QDateTime::currentDateTime().toString(QStringLiteral("hh:mm:ss"))).arg(text));
 }
 
-void adminWindow::on_actionCreateTables_triggered()
+
+void adminWindow::on_actionCreate_house_structure_triggered()
 {
-    
+    m_databaseAdmin->createHouseStructure();
 }
 
-void adminWindow::on_actionCreateStatic_triggered()
+void adminWindow::on_actionCreate_Update_devices_types_triggered()
 {
-    
+    m_databaseAdmin->createDevicesTypes();
+}
+
+void adminWindow::on_actionCreate_Update_tables_triggered()
+{
+    m_databaseAdmin->createTables();
+}
+
+void adminWindow::on_actionDrop_tables_triggered()
+{
+    m_databaseAdmin->deleteTables();
+}
+
+void adminWindow::on_actionReset_database_triggered()
+{
+    if (!m_databaseAdmin->deleteTables())
+        return;
+    if (!m_databaseAdmin->createTables())
+        return;
+    if (!m_databaseAdmin->createDevicesTypes())
+        return;
+    if (!m_databaseAdmin->createHouseStructure())
+        return;
 }
--- newSoftware/homeAdminTool/adminWindow.h
+++ newSoftware/homeAdminTool/adminWindow.h
@@ -2,11 +2,17 @@
 #define ADMINWINDOW_H
 
 #include <QMainWindow>
+#include "adminToolItem.h"
 
 namespace Ui {
 class adminWindow;
 }
 
+class databaseAdmin;
+
+/**
+ * @brief The home automation admin app.
+ */
 class adminWindow : public QMainWindow
 {
     Q_OBJECT
@@ -15,15 +21,34 @@ public:
     explicit adminWindow(QWidget *parent = 0);
     ~adminWindow();
     
-private slots:
-    void on_actionDeleteTables_triggered();
-    
-    void on_actionCreateTables_triggered();
-    
-    void on_actionCreateStatic_triggered();
-    
+private Q_SLOTS:
+
+    /**
+     * @brief Displays a message with color code and timestamp.
+     * @param The message text
+     * @param The message type
+     */
+    void displayLogMessage(const QString &text, const adminToolItem::messageType type);
+
+    /**
+     * @brief User requested updating the house structure
+     * Set the house rooms and input/output devices.
+     */
+    void on_actionCreate_house_structure_triggered();
+
+    void on_actionCreate_Update_devices_types_triggered();
+
+    void on_actionCreate_Update_tables_triggered();
+
+    void on_actionDrop_tables_triggered();
+
+    void on_actionReset_database_triggered();
+
 private:
     Ui::adminWindow *ui;
+
+    // pages
+    databaseAdmin * m_databaseAdmin;
 };
 
 #endif // ADMINWINDOW_H
