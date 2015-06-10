a512f6f20560f7b884d636fc918d78d069d616c4
Benoit Dumas <bntdumas@gmail.com>
2015-06-10 17:37:51 +0200

    Continue working on the tests

--- tests/tst_networkModuleSimulation.cpp
+++ tests/tst_networkModuleSimulation.cpp
@@ -3,53 +3,43 @@
 #include <QCoreApplication>
 #include <QTcpSocket>
 
-#include "homeNetwork.h"
+#include "homeServer.h"
 #include "clientSimulator.h"
 
-class NetworkModuleTest : public QObject
+class networkModuleSimulationTest : public QObject
 {
     Q_OBJECT
 
 public:
-    NetworkModuleTest();
+    networkModuleSimulationTest() {}
 
 private Q_SLOTS:
     void init();
     void cleanup();
 
-    // using local simulator
     void testSendLotsOfLines();
 
-    //using real esp8266 module connected via wifi
-    void testWifiConnection();
-    void testCommands();
-
-
 private:
-    homeNetwork *m_server;
+    homeServer *m_server;
     clientSimulator *m_client;
 
     void initSimulator();
-    void waitForRealClientConnected(int numberOfExpectedClients);
 };
 
-NetworkModuleTest::NetworkModuleTest()
+void networkModuleSimulationTest::init()
 {
-}
-
-void NetworkModuleTest::init()
-{
-    m_server = new homeNetwork();
+    m_server = new homeServer();
+    m_server->createTCPServer();
     QVERIFY(m_server->isListening());
 }
 
-void NetworkModuleTest::cleanup()
+void networkModuleSimulationTest::cleanup()
 {
     delete m_server;
 }
 
 // simple test,
-void NetworkModuleTest::testSendLotsOfLines()
+void networkModuleSimulationTest::testSendLotsOfLines()
 {
     initSimulator();
 
@@ -72,31 +62,7 @@ void NetworkModuleTest::testSendLotsOfLines()
     m_client = 0;
 }
 
-void NetworkModuleTest::testWifiConnection()
-{
-  //  waitForRealClientConnected(3);
-}
-
-void NetworkModuleTest::testCommands()
-{
-    waitForRealClientConnected(3);
-    m_server->resetLineCounter();
-    const int messageToSend = 100;
-    for (int i = 0; i < messageToSend; i++) {
-        m_server->sendAll("DATA");
-    }
-
-    QTimer timer;
-    timer.setSingleShot(true);
-    timer.start(90000);
-
-    while (timer.isActive())
-        QCoreApplication::instance()->processEvents();
-
-    QCOMPARE(m_server->receivedLines(), messageToSend * 3);
-}
-
-void NetworkModuleTest::initSimulator()
+void networkModuleSimulationTest::initSimulator()
 {
     QTimer timer;
     timer.setSingleShot(true);
@@ -118,24 +84,6 @@ void NetworkModuleTest::initSimulator()
     QCOMPARE(m_server->connectedClients(), 1);
 }
 
-void NetworkModuleTest::waitForRealClientConnected(int numberOfExpectedClients)
-{
-    QTimer timer;
-    timer.setSingleShot(true);
-    qDebug() << "######## NOW RESET THE MODULES ########";
-    timer.start(3000);
-    while (timer.isActive())
-        QCoreApplication::instance()->processEvents();
+QTEST_MAIN(networkModuleSimulationTest)
 
-    // wait for 5seconds, that should give enough time for clients to connect
-    timer.start(5000);
-    while (timer.isActive())
-        QCoreApplication::instance()->processEvents();
-
-
-    QCOMPARE(m_server->connectedClients(), numberOfExpectedClients);
-}
-
-QTEST_MAIN(NetworkModuleTest)
-
-#include "tst_NetworkModuleTest.moc"
+#include "tst_networkModuleSimulation.moc"
--- tests/tst_networkModuleWithHardware.cpp
+++ tests/tst_networkModuleWithHardware.cpp
@@ -3,22 +3,18 @@
 #include <QCoreApplication>
 #include <QTcpSocket>
 
-#include "homeNetwork.h"
-#include "clientSimulator.h"
+#include "homeServer.h"
 
-class NetworkModuleTest : public QObject
+class networkModuleWithHardwareTest : public QObject
 {
     Q_OBJECT
 
 public:
-    NetworkModuleTest();
+    networkModuleWithHardwareTest();
 
 private Q_SLOTS:
     void init();
     void cleanup();
-
-    // using local simulator
-    void testSendLotsOfLines();
 
     //using real esp8266 module connected via wifi
     void testWifiConnection();
@@ -26,64 +22,38 @@ private Q_SLOTS:
 
 
 private:
-    homeNetwork *m_server;
-    clientSimulator *m_client;
-
-    void initSimulator();
+    homeServer *m_server;
     void waitForRealClientConnected(int numberOfExpectedClients);
 };
 
-NetworkModuleTest::NetworkModuleTest()
+networkModuleWithHardwareTest::networkModuleWithHardwareTest()
 {
 }
 
-void NetworkModuleTest::init()
+void networkModuleWithHardwareTest::init()
 {
-    m_server = new homeNetwork();
+    m_server = new homeServer();
+    m_server->createTCPServer();
     QVERIFY(m_server->isListening());
 }
 
-void NetworkModuleTest::cleanup()
+void networkModuleWithHardwareTest::cleanup()
 {
     delete m_server;
 }
 
-// simple test,
-void NetworkModuleTest::testSendLotsOfLines()
-{
-    initSimulator();
-
-    const int messageToSend = 100;
-    //have the client send lots of lines and make sure the server got them all
-    for (int i = 0; i < messageToSend; ++i) {
-       QVERIFY(m_client->sendID());
-       QCoreApplication::instance()->processEvents();
-    }
-
-    QTimer timer;
-    timer.setSingleShot(true);
-    timer.start(3000);
-    while (timer.isActive())
-        QCoreApplication::instance()->processEvents();
-
-    QCOMPARE(m_server->receivedLines(), messageToSend);
-
-    delete m_client;
-    m_client = 0;
-}
-
-void NetworkModuleTest::testWifiConnection()
+void networkModuleWithHardwareTest::testWifiConnection()
 {
   //  waitForRealClientConnected(3);
 }
 
-void NetworkModuleTest::testCommands()
+void networkModuleWithHardwareTest::testCommands()
 {
     waitForRealClientConnected(3);
     m_server->resetLineCounter();
     const int messageToSend = 100;
     for (int i = 0; i < messageToSend; i++) {
-        m_server->sendAll("DATA");
+        m_server->sendAll(QStringLiteral("DATA"));
     }
 
     QTimer timer;
@@ -96,29 +66,7 @@ void NetworkModuleTest::testCommands()
     QCOMPARE(m_server->receivedLines(), messageToSend * 3);
 }
 
-void NetworkModuleTest::initSimulator()
-{
-    QTimer timer;
-    timer.setSingleShot(true);
-    m_client = new clientSimulator();
-    QSignalSpy spyNewData(m_client->socket(), SIGNAL(stateChanged(QAbstractSocket::SocketState)));
-
-    // connect to server, timeout 3 seconds
-    m_client->connectToServer();
-    timer.start(3000);
-
-    while (!m_client->isConnected() && timer.isActive())
-        QCoreApplication::instance()->processEvents();
-
-    //QAbstractSocket::HostLookupState
-    //QAbstractSocket::ConnectingState
-    //QAbstractSocket::ConnectedState
-    QVERIFY(spyNewData.count() == 3);
-    QVERIFY(m_client->isConnected());
-    QCOMPARE(m_server->connectedClients(), 1);
-}
-
-void NetworkModuleTest::waitForRealClientConnected(int numberOfExpectedClients)
+void networkModuleWithHardwareTest::waitForRealClientConnected(int numberOfExpectedClients)
 {
     QTimer timer;
     timer.setSingleShot(true);
@@ -136,6 +84,6 @@ void NetworkModuleTest::waitForRealClientConnected(int numberOfExpectedClients)
     QCOMPARE(m_server->connectedClients(), numberOfExpectedClients);
 }
 
-QTEST_MAIN(NetworkModuleTest)
+QTEST_MAIN(networkModuleWithHardwareTest)
 
-#include "tst_NetworkModuleTest.moc"
+#include "tst_networkModuleWithHardware.moc"
